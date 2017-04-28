ef4e62746ee5f230cb57a64e34847972581b766b
Heba Syed <hsyed2017@pingry.org>
2017-04-28 14:09:20 -0400

    Added those tests

--- palindrome.py
+++ palindrome.py
@@ -6,3 +6,13 @@ def is_palindrome(string):
 		return is_palindrome(string[1:len(string)-1])
 	else:
 		return False
+
+### Tests
+
+print("tacocat is a plandrome: {}. ".format(is_palindrome("tacocat")))
+print("racecar is a plandrome: {}. ".format(is_palindrome("racecar")))
+print(" is a plandrome: {}. ".format(is_palindrome("")))
+print("qwer is a plandrome: {}. ".format(is_palindrome("qwer")))
+print("e is a plandrome: {}. ".format(is_palindrome("e")))
+print("uu is a plandrome: {}. ".format(is_palindrome("uu")))
+print("ho is a plandrome: {}. ".format(is_palindrome("ho")))
